b5e3f23c87206d6a457e3f24cd820ee3836146c5
Hurley2017 <luciefer9062mondal@gmail.com>
2023-04-05 20:47:26 +0530

    Question1 done

--- Semester 2/Advanced Programming/Java/Assignment_1/Question1.java
+++ Semester 2/Advanced Programming/Java/Assignment_1/Question1.java
@@ -1,8 +1,18 @@
-import system.util.*
+import java.util.*;
 class Main
 {
     public static void main(String[] Tusher)
     {
-        System.out.println("Ello There!");
+        short a, b;
+        int answer;
+        Scanner buffer = new Scanner(System.in);
+        System.out.println("Question 1 : ");
+        System.out.println("Enter first short integer : ");
+        a = buffer.nextShort();
+        System.out.println("Enter second short integer : ");
+        b = buffer.nextShort();
+        answer = a + b;
+        System.out.println("The sum of the two integers is " + answer + ".");
+        buffer.close();
     }
 }
