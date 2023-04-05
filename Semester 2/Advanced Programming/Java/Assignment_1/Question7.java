b4e03fe3c38868b275064159401b41f0bc5a0377
Hurley2017 <luciefer9062mondal@gmail.com>
2023-04-05 22:39:40 +0530

    Question 7 done

---
+++ Semester 2/Advanced Programming/Java/Assignment_1/Question7.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] Tusher)
+    {
+        System.out.println("Question 7 :");
+        Scanner buffer = new Scanner(System.in);
+        String StudentName;
+        int ClassRoll;
+        double Score;
+        System.out.println("Enter Student name : ");
+        StudentName = buffer.nextLine();
+        System.out.println("Enter Student roll : ");
+        ClassRoll = buffer.nextInt();
+        System.out.println("Enter Student Score : ");
+        Score = buffer.nextDouble();
+        Student S1 = new Student();
+        Student S2 = new Student();
+        S1.Setters(StudentName, ClassRoll, Score);
+        S1.Getters();
+        S2 = S1.Copy(S2);
+        S2.Getters();
+        buffer.close();
+    }
+}
+class Student
+{
+    String StudentName;
+    int ClassRoll;
+    double Score;
+    void Setters(String StudentName, int ClassRoll, double Score)
+    {
+        this.StudentName = StudentName;
+        this.ClassRoll = ClassRoll;
+        this.Score = Score;
+    }
+    void Getters()
+    {
+        System.out.println("Name of the student is - " + StudentName + ".");
+        System.out.println("Roll of the student is - " + ClassRoll + ".");
+        System.out.println("Score of the student is - " + Score + ".");
+    }
+    Student Copy(Student Subject)
+    {
+        Subject.StudentName = this.StudentName;
+        Subject.ClassRoll = this.ClassRoll;
+        Subject.Score = this.Score;
+        return Subject;
+    }
+}
