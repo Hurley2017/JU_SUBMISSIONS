3c527878b8955766093bbfbf9c38522213cbf473
Tusher Mondal <luciefer9062hurley@gmail.com>
2023-04-11 17:09:34 +0530

    Create Question10.java
    from lab pc

---
+++ Semester 2/Advanced Programming/Java/Assignment_1/Question10.java
@@ -0,0 +1,28 @@
+//Question 10
+class Main 
+{
+    public static void main(String[] Tusher)
+    {
+        Scanner Converter = new Scanner(System.in);
+        double KiloMeter, Miles;
+        Metric One = new Metric();
+        System.out.print("Enter KiloMeter Value : ");
+        KiloMeter = Converter.nextDouble();
+        System.out.println("Converted Miles value is : " + One.KiloMeterToMiles(KiloMeter));
+        System.out.print("Enter Miles Value : ");
+        Miles = Converter.nextDouble();
+        System.out.println("Converted KiloMeter value is : " + One.MilesToKiloMeter(Miles));
+        Converter.close();
+    }
+}
+class Metric
+{
+    double KiloMeterToMiles(double KiloMeter)
+    {
+        return KiloMeter/1.5;
+    }
+    double MilesToKiloMeter(double Miles)
+    {
+        return Miles*1.5;
+    }
+}
