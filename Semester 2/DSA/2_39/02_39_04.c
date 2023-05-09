661989178f11f324a9793e63a981a25a08fb27b9
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-09 19:08:12 +0530

    circular list done!

--- Semester 2/DSA/2_39/02_39_04.c
+++ Semester 2/DSA/2_39/02_39_04.c
@@ -19,10 +19,15 @@ void displayDual(struct ClistDual *head, int size);
 int  *getDim(int size);
 int** create2Darray(int row, int coloumn);
 void displayArr(int **array2d, int row, int coloumn);
+int killcamArr(int **array2d, int row, int coloumn);
+int killcam(struct Clist *head, int size);
+int killcamDual(struct ClistDual *head, int size);
 void UI_Statements();
+void execute2darray(int **array2d, int row, int coloumn, int start,  int skip);
+void executeClist(struct Clist *head, int size, int start,  int skip);
 int main()
 {
-    int people, count = 0, sw, con = 1;
+    int people, count = 0, sw, con = 1, skip, start, survivor;
     int *dim, **array2d;
     Clist *head1 = (Clist*)malloc(sizeof(Clist));
     ClistDual *head2 = (ClistDual*)malloc(sizeof(ClistDual));
@@ -41,7 +46,15 @@ int main()
                 printf("Creating 2D Array of such . . .\n");
                 array2d = create2Darray(*(dim), *(dim+1));
                 printf("Displaying the 2D Array : \n");
-                displayArr(array2d, *(dim), *(dim+1));\
+                displayArr(array2d, *(dim), *(dim+1));
+                printf("Enter the starting point of execution : ");
+                scanf("%d", &start);
+                printf("Skip people : ");
+                scanf("%d", &skip);
+                execute2darray(array2d, *(dim), *(dim+1), start, skip);
+                printf("After all the execution the 2D array looks like this : \n");
+                survivor = killcamArr(array2d, *(dim), *(dim+1));
+                printf("%d survives.\n", survivor);
                 free(dim);
                 break;
             case 2:
@@ -49,6 +62,13 @@ int main()
                 printf("Successfully created Circular Linked-List of such . . .\n");
                 printf("Displaying the Linked-List\n");
                 display(head1, people);
+                printf("Enter the starting point of execution : ");
+                scanf("%d", &start);
+                printf("Skip people : ");
+                scanf("%d", &skip);
+                executeClist(head1, people, start, skip);
+                survivor = killcam(head1, people);
+                printf("%d survives.\n", survivor);
                 break;
             case 3:
                 createCirclularDual(head2, people);
@@ -83,6 +103,54 @@ void UI_Statements()
     printf("6) Exit\n");
     printf("Enter Choice : ");
 }
+void executeClist(struct Clist *head, int size, int start,  int skip)
+{
+    int people_alive = size;
+    int dynamic_skip = start+skip;
+    while(people_alive>1)
+    {
+        if(head->value != 0)
+        {
+            if(dynamic_skip == 0)
+            {
+                printf("[%d]-executed\n", head->value);
+                head->value = 0;
+                people_alive--;
+                dynamic_skip = skip;
+            }
+            else
+            {
+                dynamic_skip--;
+            }
+        }
+        head = head->next;
+    }
+}
+void execute2darray(int **array2d, int row, int coloumn, int start, int skip)
+{
+    int people_alive = row*coloumn;
+    int dynamic_skip = start+skip;
+    while(people_alive>1)
+    {
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<coloumn; j++)
+            {
+                if(dynamic_skip == 0 && array2d[i][j]!=0)
+                {
+                    printf("[%d]-executed\n", array2d[i][j]);
+                    array2d[i][j] = 0;
+                    dynamic_skip = skip;
+                    people_alive--;
+                }
+                if(array2d[i][j] != 0)
+                {
+                    dynamic_skip--;
+                }
+            }
+        }
+    }
+}
 void displayArr(int **array2d, int row, int coloumn)
 {
     for(int i=0; i<row; i++)
@@ -93,6 +161,27 @@ void displayArr(int **array2d, int row, int coloumn)
         }
         printf("\n");
     }
+}
+int killcamArr(int **array2d, int row, int coloumn)
+{
+    int ret;
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<coloumn; j++)
+        {
+            if(array2d[i][j])
+            {
+                printf("%d\t", array2d[i][j]);
+                ret = array2d[i][j];
+            }
+            else
+            {
+                printf("F\t");
+            }
+        }
+        printf("\n");
+    }
+    return ret;
 }
 int** create2Darray(int row, int coloumn)
 {
@@ -142,6 +231,76 @@ void display(struct Clist *head, int size)
         head = head->next;
     }
     head = temp;
+}
+int killcam(struct Clist *head, int size)
+{
+    int ret;
+    Clist *temp = head;
+    while(size-- > 0)
+    {
+        if(size == 0)
+        {
+            if(head->value == 0)
+            {
+                printf("F\n");
+            }
+            else
+            {
+                ret = head->value;
+                printf("%d\n", head->value);
+            }
+        }
+        else
+        {
+            if(head->value == 0)
+            {
+                printf("F->");
+            }
+            else
+            {
+                ret = head->value;
+                printf("%d->", head->value);
+            }
+        }
+        head = head->next;
+    }
+    head = temp;
+    return ret;
+}
+int killcamDual(struct ClistDual *head, int size)
+{
+    int ret;
+    ClistDual *temp = head;
+    while(size-- > 0)
+    {
+        if(size == 0)
+        {
+            if(head->value == 0)
+            {
+                printf("F\n");
+            }
+            else
+            {
+                ret = head->value;
+                printf("%d\n", head->value);
+            }
+        }
+        else
+        {
+            if(head->value == 0)
+            {
+                printf("F-><-");
+            }
+            else
+            {
+                ret = head->value;
+                printf("%d-><-", head->value);
+            }
+        }
+        head = head->next;
+    }
+    head = temp;
+    return ret;
 }
 void createCirclular(struct Clist *head, int size)
 {
