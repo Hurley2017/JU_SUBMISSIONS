0a639410c666c6eea86e5fcb2ddb9ccc87feaeb5
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-12 10:00:48 +0530

    7 recovered! version mismatch!

--- Semester 2/DSA/3_39/03_39_07.c
+++ Semester 2/DSA/3_39/03_39_07.c
@@ -15,17 +15,62 @@ int pop(Stack *s);
 int peek(Stack *s);
 int isOperator(char c);
 int Precedence(char c);
-void conversion(char *infix, char *postfix);
 int isEmpty(Stack s);
+int evaluation(char *postfix);
+int whichop(char ch)
+{
+    if(ch == '+')
+        return 1;
+    if(ch == '-')
+        return 2;
+    if(ch == '/')
+        return 3;
+    if(ch == '*')
+        return 4;
+}
 int main()
 {
-    char *infix = (char*)malloc(max*sizeof(char));
     char *postfix = (char*)malloc(max*sizeof(char));
-    printf("Enter the infix expression : ");
-    fgets(infix, max, stdin);
-    conversion(infix, postfix);
-    printf("PostFix Expression is : %s\n", postfix);
+    printf("Enter the postfix expression : ");
+    fgets(postfix, max, stdin);
+    printf("Evaluated : %d\n", evaluation(postfix));
     return 0;
+}
+int evaluation(char *postfix)
+{
+    printf("%s\n", postfix);
+    int counter = 0;
+    Stack s;
+    init(&s, max);
+    while(*(postfix+counter) != '\0')
+    {
+        display(&s);
+        if(isOperator(*(postfix + counter)))
+        {
+            switch (whichop(*(postfix+counter)))
+            {
+                case 1: 
+                    push(&s, (pop(&s)+pop(&s)));
+                    break;
+                case 2:
+                    push(&s, (pop(&s)-pop(&s)));
+                    break;
+                case 3:
+                    push(&s, (pop(&s)/pop(&s)));
+                    break;
+                case 4:
+                    push(&s, (pop(&s)+pop(&s)));
+                    break;
+            }
+        }
+        else
+        {
+            push(&s, *(postfix+counter)-48);
+        }
+        counter++;
+    }
+    printf("%d", pop(&s));
+    return pop(&s);
 }
 int isEmpty(Stack s)
 {
@@ -42,51 +87,6 @@ int Precedence(char c)
     else if (c == '*' || c == '/')
         return 2;
     return 0;
-}
-void conversion(char *infix, char *postfix)
-{
-    Stack stack;
-    init(&stack, max);
-    int i, j = 0;
-
-    for (i = 0; infix[i] != '\0'; i++)
-    {
-        char ch = infix[i];
-        if (isalnum(ch))
-        {
-            postfix[j++] = ch;
-        }
-        else if (ch == '(')
-        {
-            push(&stack, ch);
-        }
-        else if (ch == ')')
-        {
-            while (!isEmpty(stack) && peek(&stack) != '(')
-            {
-                postfix[j++] = pop(&stack);
-            }
-            if (!isEmpty(stack) && peek(&stack) != '(')
-            {
-                printf("Invalid expression\n");
-                exit(1);
-            }
-            pop(&stack);
-        }
-        else if (isOperator(ch))
-        {
-            while (!isEmpty(stack) && Precedence(ch) <= Precedence(peek(&stack)))
-            {
-                postfix[j++] = pop(&stack);
-            }
-            push(&stack, ch);
-        }
-    }
-    while (!isEmpty(stack))
-    {
-        postfix[j++] = pop(&stack);
-    }
-    postfix[j] = '\0';
 }
 void display(Stack *s)
 {
